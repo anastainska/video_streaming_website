c359099470b3caedd26bd1cfeac4cfc5ee073514
Anastasiia Kainska <anastasia.kajinska@gmail.com>
2024-01-13 11:26:45 +0200

    updated admin.py

--- my_website_app/admin.py
+++ my_website_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Admin, Subscriber, Show, Folder
 
 # Register your models here.
+
+
+class SubscriberAdmin(UserAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User)
+admin.site.register(Admin)
+admin.site.register(Subscriber, SubscriberAdmin)
+admin.site.register(Show)
+admin.site.register(Folder)
