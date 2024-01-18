439fed0cd6a69daef0ee1847ccc6fb7027c19f43
anastainska <anastasia.kajinska@gmail.com>
2024-01-18 15:05:24 +0200

    added changing and resetting password

--- video_streaming_service/urls.py
+++ video_streaming_service/urls.py
@@ -34,4 +34,22 @@ urlpatterns = [
     path('show/<int:show_id>/add_to_favorites/', favourite_add, name='favourite_add'),
     path('show/<int:show_id>/remove_from_favorites/', remove_from_favorites, name='remove_from_favorites'),
     path('favourites/', favourite_list, name='favourites'),
+
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
