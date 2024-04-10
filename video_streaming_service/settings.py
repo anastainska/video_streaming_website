da1603f4b3046f946997e562bc53110dfe7c8791
anastainska <anastasia.kajinska@gmail.com>
2024-04-10 18:33:37 +0300

    trying to deploy on aws

--- video_streaming_service/settings.py
+++ video_streaming_service/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.220.178.56']
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'  # During development only, needs change
